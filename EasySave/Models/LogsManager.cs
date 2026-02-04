4d1bdf11138cd29c03f49c8b2a8587ebbec86155
VictorLabeille <Victor@YOGAPRO7I>
2026-02-04 17:25:40 +0100

    [FEATURE] - LogsManager finished and EasyLog added as a dependancy (reference is VS)

--- EasySave/Models/LogsManager.cs
+++ EasySave/Models/LogsManager.cs
@@ -1,14 +1,18 @@
 using EasySave.Interfaces;
+using EasyLog;
 
 namespace EasySave.Models;
 
 public class LogsManager:IEventListener
 {
+    // Transform and transfer BackupInfos to Logger
     public void Update(BackupInfo data)
     {
-        throw new NotImplementedException();
+        string logText = this._toJson(data);
+        Logger.GetInstance().Log(logText);
     }
 
+    // Transform BackupInfo data into a JSON string
     private string _toJson(BackupInfo data)
     {
         return $@"{{
@@ -16,7 +20,7 @@ public class LogsManager:IEventListener
            ""FileSource"": ""{data.CurrentCopyInfo.Source.Replace(@"\", @"\\")}"",
            ""FileTarget"": ""{data.CurrentCopyInfo.Destination.Replace(@"\", @"\\")}"",
            ""FileSize"": {data.CurrentCopyInfo.Size},
-           ""FileTransferTime"": {je vais devoir faire end time moins start time},
+           ""FileTransferTime"": {(data.CurrentCopyInfo.EndTime - data.CurrentCopyInfo.StartTime).TotalMilliseconds},
            ""Time"": ""{DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")}""
          }}";
     }
