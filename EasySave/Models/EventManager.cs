b8a0440c4d98bb61bccb651714a212bba7f4185d
VictorLabeille <Victor@YOGAPRO7I>
2026-02-04 17:31:41 +0100

    [WIP] - EvenManager GetInstance(), Update() and constructor

--- EasySave/Models/EventManager.cs
+++ EasySave/Models/EventManager.cs
@@ -1,20 +1,27 @@
+using EasyLog;
 using EasySave.Interfaces;
 
 namespace EasySave.Models;
 
 public class EventManager
 {
-    private EventManager _instance;
+    private static EventManager _instance;
     private List<IEventListener> _subscribers;
 
-    public EventManager GetInstance()
+    // Create an EventManager singleton or return the existing one
+    public static EventManager GetInstance()
     {
-        throw new NotImplementedException();
+        if (_instance == null)
+        {
+            _instance = new EventManager();
+        }
+
+        return _instance;
     }
 
     private EventManager()
     {
-        throw new NotImplementedException();
+        _subscribers = new List<IEventListener>();
     }
 
     public void Subscribe(IEventListener listener)
@@ -22,8 +29,12 @@ public class EventManager
         throw new NotImplementedException();
     }
 
+    // Notify all subscribers with BackupInfo
     public void Update(BackupInfo data)
     {
-        throw new NotImplementedException();
+        foreach(IEventListener listener in _subscribers)
+        {
+            listener.Update(data);
+        }
     }
 }
