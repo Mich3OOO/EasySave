758b1f0f163c8a740220e5cfdd8feeeb82ef5f04
Mich3000 <sepulmichael@hotmail.com>
2026-02-04 21:21:20 +0100

    feat(ViewModel): ViewModel logic implementation
    Added all function that the user will need (hopfully)

--- EasySave/ViewModels/ConfigViewModel.cs
+++ EasySave/ViewModels/ConfigViewModel.cs
@@ -1,41 +1,74 @@
 using EasySave.Models;
 
-namespace EasySave.View;
+namespace EasySave.ViewModels;
 
 public class ConfigViewModel
 {
     private Config _config;
 
+
+    public ConfigViewModel()
+    {
+        _config = Config.S_GetInstance();
+    }
+    
     public void SaveConfig()
     {
-        throw new NotImplementedException();
+        _config.SaveConfig();
     }
-    public void SetLanguage(string lang)
+    public void SetLanguage(Languages lang)
     {
-        throw new NotImplementedException();
+        _config.Language = lang;
     }
-    public void ChangeJobSource(string jobSource, string newJobSource)
+    public void ChangeJobSource(string jobName, string newJobSource)
     {
-        throw new NotImplementedException();
+        SavedJob? job = _config.GetJob(jobName);
+        if (job != null)
+        { 
+            job.Source = newJobSource;
+            _config.UpdateJob(jobName,job); 
+        }
+       
     }
     public void ChangeJobName(string oldJobName, string newJobName)
     {
-        throw new NotImplementedException();
+        SavedJob? job = _config.GetJob(oldJobName);
+        if (job != null)
+        { 
+            job.Name = newJobName;
+            _config.UpdateJob(oldJobName,job); 
+        }
     }
     public void ChangeJobDestination(string jobName, string newDestination)
     {
-        throw new NotImplementedException();
+        SavedJob? job = _config.GetJob(jobName);
+        if (job != null)
+        { 
+            job.Destination = newDestination;
+            _config.UpdateJob(jobName,job); 
+        }
     }
     public void CreateJob(string name, string source, string destination)
     {
-        throw new NotImplementedException();
+        List<SavedJob> savedJobs = _config.SavedJobs;
+        _config.AddJob(new SavedJob(savedJobs.Count > 0 ?savedJobs.Max(j=> j.Id) + 1: 1 ,name, source, destination));
     }
     public string[] GetJobsNames()
     {
-        throw new NotImplementedException();
+        
+        return _config.SavedJobs.Select(s => s.Name).ToArray();
     }
-    public SavedJob GetJob(string name)
+    public SavedJob? GetJob(string name)
     {
-        throw new NotImplementedException();
+        return _config.GetJob(name);
+    }
+    
+    public void DeleteJob(string name)
+    {
+        SavedJob? job = _config.GetJob(name);
+        if (job != null)
+        { 
+            _config.DeleteJob(job);
+        }
     }
 }
