5da55931b6362c5ff30b43f9d58ff5c4338765d2
Charlie <charliewiatr@gmail.com>
2026-02-05 15:56:38 +0100

    "Merge": /log into backup

--- EasySave/Models/LogsManager.cs
+++ EasySave/Models/LogsManager.cs
@@ -1,11 +1,32 @@
 using EasySave.Interfaces;
+using EasyLog;
 
 namespace EasySave.Models;
 
-public class LogsManager:IEventListener
+public class LogsManager : IEventListener
 {
+    // Constructor that automatically subscribes to EventManager
+    public LogsManager()
+    {
+        EventManager.GetInstance().Subscribe(this);
+    }
+    // Transform and transfer BackupInfos to Logger
     public void Update(BackupInfo data)
     {
-        throw new NotImplementedException();
+        string logText = this._toJson(data);
+        Logger.GetInstance().Log(logText);
+    }
+
+    // Transform BackupInfo data into a JSON string
+    private string _toJson(BackupInfo data)
+    {
+        return $@"{{
+           ""Name"": ""{data.SavedJobInfo.getName()}"",
+           ""FileSource"": ""{data.CurrentCopyInfo.Source.Replace(@"", @"\")}"",
+           ""FileTarget"": ""{data.CurrentCopyInfo.Destination.Replace(@"", @"\")}"",
+           ""FileSize"": {data.CurrentCopyInfo.Size},
+           ""FileTransferTime"": {(data.CurrentCopyInfo.EndTime - data.CurrentCopyInfo.StartTime).TotalMilliseconds},
+           ""Time"": ""{DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")}""
+         }}";
     }
 }
