15c9b056cb3c20a7bdf784410aad5745560a9610
VictorLabeille <Victor@YOGAPRO7I>
2026-02-11 14:09:15 +0100

    [WIP] - LogsManager updated for multiple formats

--- EasySave/Models/LogsManager.cs
+++ EasySave/Models/LogsManager.cs
@@ -5,6 +5,9 @@ namespace EasySave.Models;
 
 public class LogsManager : IEventListener   // Class representing the logs manager, implementing the IEventListener interface
 {
+
+    Config _config = Config.S_GetInstance();
+
     // Constructor that automatically subscribes to EventManager
     public LogsManager()
     {   
@@ -13,7 +16,20 @@ public class LogsManager : IEventListener   // Class representing the logs manag
     // Transform and transfer BackupInfos to Logger
     public void Update(BackupInfo data)
     {
-        string logText = this._toJson(data);
+        string logText = ""; // Initializing the log text variable
+        LogsFormats formats = _config.GetLogsFormat(); // Retrieving the logs format from config
+        if (formats == LogsFormats.Json) // If the format is JSON, transform the data into a JSON string and log it
+        {
+            logText = this._toJson(data);
+        }
+        if (formats == LogsFormats.Xml) // If the format is XML, transform the data into an XML string and log it
+        {
+            logText = this._toXml(data);
+        }
+        else // If the format isn't reconized or sent, to Json (default)
+        {
+            logText = this._toJson(data);
+        }
         Logger.GetInstance().Log(logText);
     }
 
@@ -38,5 +54,28 @@ public class LogsManager : IEventListener   // Class representing the logs manag
            ""FileTransferTime"": {(data.CurrentCopyInfo.EndTime - data.CurrentCopyInfo.StartTime).TotalMilliseconds},
            ""Time"": ""{DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")}""
          }}";
+    }
+
+    // Transform BackupInfo data into a XML string
+    private string _toXml(BackupInfo data)
+    {
+        // Check if the data sent
+        if (data == null)
+        {
+            throw new ArgumentNullException(nameof(data), "Backup data cannot be null.");
+        }
+        if (data.SavedJobInfo == null || data.CurrentCopyInfo == null)
+        {
+            throw new ArgumentException("Invalid backup data structure.");
+        }
+        // Create 'XML' string with infos from data
+        return $@"<Log>
+                    <Name>{data.SavedJobInfo.GetName()}</Name>
+                    <FileSource>{data.CurrentCopyInfo.Source}</FileSource>
+                    <FileTarget>{data.CurrentCopyInfo.Destination}</FileTarget>
+                    <FileSize>{data.CurrentCopyInfo.Size}</FileSize>
+                    <FileTransferTime>{(data.CurrentCopyInfo.EndTime - data.CurrentCopyInfo.StartTime).TotalMilliseconds}</FileTransferTime>
+                    <Time>{DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")}</Time>
+                </Log>";
     }
 }
