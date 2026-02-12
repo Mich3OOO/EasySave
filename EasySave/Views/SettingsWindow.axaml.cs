8b9d7b08a8693fff25378d941f6fc8044d1a5c72
Nolan JOURDAN <nolan.jourdan@viacesi.fr>
2026-02-12 10:18:49 +0100

    feat : improves Settings, added a menu for all the settings

---
+++ EasySave/Views/SettingsWindow.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace EasySave.Views;
+
+public partial class SettingsWindow : Window
+{
+    public SettingsWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void CloseButton_Click(object? sender, RoutedEventArgs e)
+    {
+        // Plus tard, on sauvegardera les données ici
+        this.Close();
+    }
+}
