09b99cb794b6bd709e04803a2caabe4dac804565
Nolan JOURDAN <nolan.jourdan@viacesi.fr>
2026-02-12 09:56:43 +0100

    Feat : Integrating the first GUI Interface

--- EasySave/ViewModels/MainWindowViewModel.cs
+++ EasySave/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,30 @@
-﻿namespace EasySave.ViewModels;
+﻿using System.Diagnostics;
+
+namespace EasySave.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
     public string Greeting { get; } = "Welcome to EasySave!";
+
+    public void CreateJob()
+    {
+        // Plus tard : On ouvrira une fenêtre ou un formulaire pour demander Source/Cible
+        Debug.WriteLine("Action : L'utilisateur veut CRÉER un job.");
+    }
+
+    public void RunJob()
+    {
+        // Plus tard : On regardera quel job est sélectionné dans la liste et on le lancera
+        Debug.WriteLine("Action : L'utilisateur veut LANCER un job.");
+    }
+
+    public void EditJob()
+    {
+        Debug.WriteLine("Action : L'utilisateur veut MODIFIER un job.");
+    }
+
+    public void DeleteJob()
+    {
+        Debug.WriteLine("Action : L'utilisateur veut SUPPRIMER un job.");
+    }
 }
