b063b59e1c16d249133203880839ebeebce081ef
VictorLabeille <Victor@YOGAPRO7I>
2026-02-12 09:00:40 +0100

    [FEATURE] - TimeToEncrypt added to CopyInfo.cs

--- EasySave/Models/CopyInfo.cs
+++ EasySave/Models/CopyInfo.cs
@@ -7,6 +7,7 @@ public class CopyInfo   // Class representing information about a file copy oper
     public DateTime StartTime;
     public long Size;
     public DateTime EndTime;
+    public int TimeToEncrypt; // Time to encrypt the file (in milliseconds)
 
     public CopyInfo()
     {
@@ -15,6 +16,7 @@ public class CopyInfo   // Class representing information about a file copy oper
         StartTime = DateTime.MinValue;
         Size = 0;
         EndTime = DateTime.MinValue;
+        TimeToEncrypt = 0;
         // Empty constructor
     }
     public CopyInfo(string source, string destination, DateTime startTime, int size, DateTime endTime)
@@ -24,5 +26,16 @@ public class CopyInfo   // Class representing information about a file copy oper
         this.StartTime = startTime;
         this.Size = size;
         this.EndTime = endTime;
+    }
+
+    // Overload of the constructor to include the time to encrypt the file, previous one note deleted because not every files are encrypted
+    public CopyInfo(string source, string destination, DateTime startTime, int size, DateTime endTime, int timeToEncrypt)
+    {
+        this.Source = source;
+        this.Destination = destination;
+        this.StartTime = startTime;
+        this.Size = size;
+        this.EndTime = endTime;
+        this.TimeToEncrypt = timeToEncrypt;
     }
 }
