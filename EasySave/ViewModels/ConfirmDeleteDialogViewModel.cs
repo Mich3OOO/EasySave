740860aff074ba5cf8d5d1f97d34777277246386
VictorLabeille <Victor@YOGAPRO7I>
2026-02-13 11:41:03 +0100

    [WIP] - ConfirmDelete Window translated

--- EasySave/ViewModels/ConfirmDeleteDialogViewModel.cs
+++ EasySave/ViewModels/ConfirmDeleteDialogViewModel.cs
@@ -6,6 +6,14 @@ namespace EasySave.ViewModels;
 
 public class ConfirmDeleteDialogViewModel : ViewModelBase
 {
+    public LanguageViewModel LanguageViewModel { get; }
+
+    public string T_confirm_deletion => LanguageViewModel.GetTranslation("confirm_deletion");
+    public string T_are_you_sure_deletion => LanguageViewModel.GetTranslation("are_you_sure_deletion");
+    public string T_irreversible_action => LanguageViewModel.GetTranslation("irreversible_action");
+    public string T_cancel => LanguageViewModel.GetTranslation("cancel");
+    public string T_delete => LanguageViewModel.GetTranslation("delete");
+
     private string _jobName = string.Empty;
 
     public string JobName
@@ -23,6 +31,9 @@ public class ConfirmDeleteDialogViewModel : ViewModelBase
     {
         CancelCommand = new RelayCommand(Cancel);
         ConfirmCommand = new RelayCommand(Confirm);
+
+        string dictionaryPath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "dictionary.json");
+        LanguageViewModel = new LanguageViewModel(dictionaryPath);
     }
 
     private void Cancel()
