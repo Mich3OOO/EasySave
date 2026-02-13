bc7ea7a32d0cfa1fec6b80f7122bfec41c751e94
Charlie <charliewiatr@gmail.com>
2026-02-13 13:34:47 +0100

    FIX: Fix ui problem => duplicate button in JobSettings, and add Password Type

--- EasySave/ViewModels/RunJobsViewModel.cs
+++ EasySave/ViewModels/RunJobsViewModel.cs
@@ -8,12 +8,19 @@ namespace EasySave.ViewModels;
 public class RunJobsViewModel : ViewModelBase
 {
     private bool _isDifferential = false;
+    private string _password = string.Empty;
     public SavedJob Job { get; }
 
     public bool IsDifferential
     {
         get => _isDifferential;
         set => SetProperty(ref _isDifferential, value);
+    }
+
+    public string Password   // Property for the password path, with getter and setter that raises property change notifications
+    {
+        get => _password;
+        set => SetProperty(ref _password, value);
     }
 
     public ICommand ConfirmCommand { get; }
