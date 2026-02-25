a434d70821866d8156691d225456db8ee5f7c755
VictorLabeille <victormarie0268@gmail.com>
2026-02-25 13:47:34 +0100

    [FEATURE] - MSTest added

---
+++ EasySave.Test/Config.Test.cs
@@ -0,0 +1,14 @@
+﻿using EasySave.Models;
+namespace EasySave.Test
+{
+    [TestClass]
+    public sealed class TestConfig
+    {
+        private readonly Config _config = Config.GetInstance();
+
+        [TestMethod]
+        public void TestSetDefaultConfig()
+        {
+        }
+    }
+}
